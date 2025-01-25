94b0792abb3474a43f0d49bab3daefff1b6f44b9
CamiloMarin <camilomv1234@gmail.com>
2025-01-24 23:10:08 -0500

    Movimiento Final de personaje

--- Assets/Code/Scripts/Player.cs
+++ Assets/Code/Scripts/Player.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float _velocity = 5f; // Velocidad del personaje
-    public float _jumpForce = 10f; // Fuerza del salto
     private Rigidbody2D _rb; // Componente del rb
     private bool _isGrounded; // bool del suelo
 
-    
+    // Velocidad ******************************************************************************
+    // Estas variables se utilizan para el movimiento del personaje
+
+    public float _actualVelocity = 5f; // Velocidad del personaje
+    public float _jumpForce = 10f; // Fuerza del salto
+    [SerializeField] private float _acceleration = 5f;      // Acceleration rate  
+    [SerializeField] private float _topSpeed = 5f;         // Maximum speed  
+    [SerializeField] private float _deceleration = 10f;    // Deceleration rate  
+
+    // Muerte ************************************************************************************
+    // Estas variables se utilizan para implementar la logica de la muerte del personaje
+
+    private bool _isDead =  false; // Estado del muerte del personaje
 
     // Start is called before the first frame update
     void Start()
-    {
+    {        
         // Capturamos el elemento del RigidBody 2d
         _rb = GetComponent<Rigidbody2D>();
     }
@@ -22,10 +32,29 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Jump 
+        if (Input.GetKeyDown(KeyCode.W) && _isGrounded)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
+        }
+
+        PlayerMovement();
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // Logica de Movimiento
+
+    private void PlayerMovement()
+    {
+
         // Movimiento Horizontal
         float _moveInput = Input.GetAxis("Horizontal"); // Las flechas de A/D & Las Flechas Left/Right 
-        _rb.velocity = new Vector2(_moveInput * _velocity, _rb.velocity.y);
-
+        
         // Cambio del sprite dependiendo de la dirección
         if (_moveInput > 0)
         {
@@ -33,32 +62,42 @@ public class Player : MonoBehaviour
         }
         else if (_moveInput < 0)
         {
-            transform.localScale = new Vector3(-1,1,1);
+            transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        // Jump 
-        if (Input.GetKeyDown(KeyCode.W) && _isGrounded)
+        if (_moveInput != 0)
         {
-            
-            _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
+            _actualVelocity = Mathf.MoveTowards(_actualVelocity, _moveInput * _topSpeed, _acceleration * Time.deltaTime);
         }
+        else
+        {
+            _actualVelocity = Mathf.MoveTowards(_actualVelocity, 0, _deceleration * Time.deltaTime);
+        }
+
+        _rb.velocity = new Vector2(_actualVelocity, _rb.velocity.y);
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Entra en colisión con el suelo
         if (collision.gameObject.CompareTag("Ground"))
         {
             _isGrounded = true;
-            Debug.Log("True");
+        }
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            CharacterDeath(); // se muere el personaje
         }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
+        // Sale de la colisión con el suelo
         if (collision.gameObject.CompareTag("Ground"))
         {
             _isGrounded = false;
-            Debug.Log("False"); //
         }
 
     }
@@ -66,5 +105,10 @@ public class Player : MonoBehaviour
     public void CharacterDeath()
     {
         // Terminar el juego
+        _isDead = true;
+
+
     }
+
+
 }
