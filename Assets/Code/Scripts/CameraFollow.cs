4dffc26b187e9e9212562ddca91507b102a0cd36
CamiloMarin <camilomv1234@gmail.com>
2025-01-25 03:22:39 -0500

    Ajustes a los movimientos de cámara
    Nuevos elementos en los movimientos de cámara para que se vean más fluido

--- Assets/Code/Scripts/CameraFollow.cs
+++ Assets/Code/Scripts/CameraFollow.cs
@@ -6,6 +6,8 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Vector3 _offset = new Vector3 (0, 0, -5f);
     [SerializeField] private float _smoothTime = 0.25f;
+    [SerializeField] private float _decreaseDistance = 0.5f;
+   
     private Vector3 _velocity = Vector3.zero;
 
     [SerializeField] private Transform _target;
@@ -14,6 +16,7 @@ public class CameraFollow : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 targetPosition =  new Vector3 (this.gameObject.transform.position.x, _target.position.y, this.gameObject.transform.position.z) - _offset;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, _smoothTime);
+        Vector3 modifiedVelocity = _velocity - new Vector3(0, _decreaseDistance, 0);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref modifiedVelocity, _smoothTime);
     }
 }
