cfd7d9529c483e3ebbf18f52d4d34a1b27b6bbbb
IsmeMau <maury.maturanalo@gmail.com>
2025-01-25 12:27:04 -0500

    M
    MAS ERRORES

---
+++ Assets/Code/Scripts/ChangeScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+        /*NOSE*/
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
