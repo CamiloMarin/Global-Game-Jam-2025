e38a5435a575ac689ba3b7ba97aaa59ba6ff4872
CamiloMarin <camilomv1234@gmail.com>
2025-01-24 21:38:05 -0500

    Movimiento del personaje
    - Movimiento con las teclas de movimiento horizontal
    
    - Lógica de Salto

--- Assets/Code/Scripts/Player.cs
+++ Assets/Code/Scripts/Player.cs
@@ -1,30 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private int velocity = 0;
+    public float _velocity = 5f; // Velocidad del personaje
+    public float _jumpForce = 10f; // Fuerza del salto
+    private Rigidbody2D _rb; // Componente del rb
+    private bool _isGrounded; // bool del suelo
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
-
+        // Capturamos el elemento del RigidBody 2d
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Movimiento Horizontal
+        float _moveInput = Input.GetAxis("Horizontal"); // Las flechas de A/D & Las Flechas Left/Right 
+        _rb.velocity = new Vector2(_moveInput * _velocity, _rb.velocity.y);
+
+        // Cambio del sprite dependiendo de la dirección
+        if (_moveInput > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (_moveInput < 0)
+        {
+            transform.localScale = new Vector3(-1,1,1);
+        }
+
+        // Jump 
+        if (Input.GetKeyDown(KeyCode.W) && _isGrounded)
+        {
+            
+            _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = true;
+            Debug.Log("True");
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = false;
+            Debug.Log("False"); //
+        }
 
     }
 
-    private void FixedUpdate()
+    public void CharacterDeath()
     {
-        
-    }
-
-    public void Movement()
-    {
-
+        // Terminar el juego
     }
 }
